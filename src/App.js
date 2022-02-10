59cdd4f69270cd92e343b447f59f6935e09c671b
Aidan Mackey <57772122+A-Mackey@users.noreply.github.com>
2022-02-10 13:45:59 -0800

    Initialize the website

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>Testing</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ffffff',
+        secondary: '#0f0f0f',
+      },
+    },
+  },
+  plugins: [],
+}
