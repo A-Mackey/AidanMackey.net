0d8fcd95446fea73aeabba1970578534a373d60e
Aidan Mackey <57772122+A-Mackey@users.noreply.github.com>
2022-02-12 18:25:02 -0800

    Added footer bar

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,24 +1,16 @@
 import '../CSS/Home.scss'
 
-import Sphere from '../Components/Sphere'
+import Shape from '../Components/Shape'
 import { useEffect, useState } from 'react';
 
 function Home(props) {
-  const [sphereColor, setSphereColor] = useState("#ffffff");
+  const [shapeColor, setShapeColor] = useState("#ffffff");
+  const [shape, setShape] = useState("sphere")
   const sphereColorDimDelta = 900;
 
   const title = "[ aidan mackey ]"
   const subTitle = "[ developer | student | producer ]"
   const yearsOfExperience = new Date().getFullYear() - 2019;
-  
-  const body = `With [ ${yearsOfExperience} years ] of software developer experience and 3 years at [ Santa Clara University ] under my belt, I have been able to work at many different companies including my own [ startup ] and have worked on many projects. This website will highlight the more notable ones.`
-
-  const highlights = [
-    { highlight: "3 years", subHighlight: "of developer experience" },
-    { highlight: "3rd year", subHighlight: "Santa Clara Student" },
-    { highlight: "2021", subHighlight: " ACM hackathon winner" },
-  ]
-
 
   const Socials = [
     { id: 0,    name: "LinkedIn",    ref: require("../Pictures/LinkedIn.png"),  link:"https://www.linkedin.com/in/aidanmackey/"    },
@@ -28,8 +20,8 @@ function Home(props) {
 
   useEffect(() => {
       function handleResize() {
-          if (window.innerWidth < sphereColorDimDelta) { setSphereColor("#333");}
-          if (window.innerWidth >= sphereColorDimDelta) { setSphereColor("#fff"); }
+          if (window.innerWidth < sphereColorDimDelta && shapeColor != "#333") { setShapeColor("#333");}
+          if (window.innerWidth >= sphereColorDimDelta && shapeColor != "#fff") { setShapeColor("#fff"); }
       }
       window.addEventListener('resize', handleResize)
       handleResize();
@@ -46,7 +38,7 @@ function Home(props) {
         <div className='homeSocials'>
           {
             Socials.map((item, index) => (
-              <form target="_blank" action={ item.link }>
+              <form key={index} target="_blank" action={ item.link }>
                 <button key={index} className='homeSocialButton'>
                   <img src={item.ref} alt="" className='homeSocialButtonImage'/>
                 </button>
@@ -55,22 +47,30 @@ function Home(props) {
           }
         </div>
 
-        {/* <div className='homeBody'>
-          {
-            highlights.map((item, index) => (
-              <li>[ {item.highlight} ] { item.subHighlight}</li>
-            ))
-          }
-        </div> */}
-
-        <div className='homeBody'>With <span className='highlightText'> {yearsOfExperience} years</span> of software developer experience and 3 years at <span className='highlightText'>Santa Clara University</span> under my belt, I have been able to work at many different companies including my own <span className='highlightText'>startup</span> and have worked on many projects. This website will highlight the more notable ones.</div>
+        <div className='homeBody'>
+          After <span className='highlightText'> {yearsOfExperience} years</span> of software developer experience and 3 years at <span className='highlightText'>Santa Clara University</span>, I have been able to work at many different companies including my own <span className='highlightText'>startup</span> and have worked on many different projects.
+        </div>
       </div>
       <div
         className='homeRightSide'
         id='sphereDiv'
       > 
-        <Sphere className='homeSphereObject' color={ sphereColor}/>
+        <Shape className='homeSphereObject' color={shapeColor} shape={shape} />
       </div>
+
+      <div className='changeShapeButtonsParent'>
+          <div className='changeShapeButtons'>
+            <button className='changeShapeButton' onClick={() => setShape("sphere")}>
+              [ Sphere ]
+            </button>
+            <button className='changeShapeButton' onClick={() => setShape("cube")}>
+              [ Cube ]
+            </button>
+            <button className='changeShapeButton' onClick={() => setShape("cone")}>
+              [ Cone ]
+            </button>
+          </div>
+        </div>
 
     </div>
   );
