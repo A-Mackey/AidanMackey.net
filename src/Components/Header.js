6e4c7379704a4873db6f9f0d0d99d6aa393d7481
Aidan Mackey <57772122+A-Mackey@users.noreply.github.com>
2022-06-14 00:12:10 -0700

    Fix CSS for header and contact page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,21 +11,25 @@ function Header() {
 	];
 
 	return (
-		<div className="headerParent">
-			<div className="headerTitle">{header}</div>
+		<div className="header-wrapper">
+			<div className="header-parent-wrapper">
+				<div className="header-parent">
+					<div className="header-title">{header}</div>
 
-			<div className="headerButtonParent">
-				{links.map((item, index) => (
-					<button
-						key={index}
-						onClick={() => (document.location = item.ref)}
-						className="headerButton"
-					>
-						{beginTag}
-						{item.title}
-						{endTag}
-					</button>
-				))}
+					<div className="header-button-parent">
+						{links.map((item, index) => (
+							<button
+								key={index}
+								onClick={() => (document.location = item.ref)}
+								className="header-button"
+							>
+								{beginTag}
+								{item.title}
+								{endTag}
+							</button>
+						))}
+					</div>
+				</div>
 			</div>
 		</div>
 	);
