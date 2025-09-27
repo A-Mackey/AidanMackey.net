41b3ec88a9dd32a845169dcf39761f4f7b3a821e
Aidan Mackey <57772122+A-Mackey@users.noreply.github.com>
2025-09-27 00:44:56 +0000

    Remove turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.externals = [...config.externals, 'prom-client'];
+  webpack: (config, { nextRuntime }) => {
+    if (nextRuntime === "nodejs") {
+      config.externals = [...config.externals, "prom-client"];
+    }
     return config;
-  }
+  },
 };
 
 export default nextConfig;
